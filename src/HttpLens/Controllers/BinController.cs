99888b69ca7b458f769a7a51b267840c9e26358a
Vadim Novitskii <novvad@gmail.com>
2022-02-26 20:35:10 -0800

    Initial commit

---
+++ src/HttpLens/Controllers/BinController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpLens.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BinController : ControllerBase
+    {
+    }
+}
